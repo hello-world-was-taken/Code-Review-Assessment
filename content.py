af82b258b0676c7f9eaf4485c3d9a5873e3d5d34
Simon Mekonnen <simonmekonnen80@gmail.com>
2023-03-27 10:44:40 -0800

    add content to content.py from snippet_3

--- content.py
+++ content.py
@@ -1,5 +1,15 @@
-def calculateArea(radius):
-    return 3.14 * radius ** 2
+def get_square(number):
+    return number ** 2
 
-def divide_numbers(a, b):
-    return a / 
+def get_cubes(numbers):
+    cubes = []
+    for number in numbers:
+        cubes.append(number ** 3)
+    return cubes
+
+my_numbers = [1, 2, 3, 4, 5]
+squares = []
+for number in my_numbers:
+    squares.append(get_square(number))
+
+cubes = get_cubes(my_numbers)
