8ab6ac50605701559efea951a0d9fa997c2e773f
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-03-27 11:00:01 -0800

    add content to content.py from snippet_1 (#3)
    * add content to content.py from snippet_1
    
    * add content to content.py from snippet_3
    
    * FIX BUG
    
    ---------
    
    Co-authored-by: hello_world <86287727+hello-world-was-taken@users.noreply.github.com>

--- content.py
+++ content.py
@@ -1,3 +1,19 @@
+def get_square(numbers):
+    square = []
+    for number in numbers:
+        squares.append(number ** 2)
+    return square
+
+def get_cubes(numbers):
+    cubes = []
+    for number in numbers:
+        cubes.append(number ** 3)
+    return cubes
+
+my_numbers = [1, 2, 3, 4, 5]
+squares = get_square(my_numbers)
+cubes = get_cubes(my_numbers)
+
 count = 0
 
 def multiply(x, y):
